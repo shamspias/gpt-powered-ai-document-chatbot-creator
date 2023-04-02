2e27982ccea173f06ff087eec9089a3ee529585e
shamspias <shamsuddinpias0@gmail.com>
2023-04-02 17:11:53 +0600

    refactor question

--- document_ai_qa/urls.py
+++ document_ai_qa/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('question_answering.urls')),
-]
+                  path('admin/', admin.site.urls),
+                  path('api/', include('question_answering.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- question_answering/models.py
+++ question_answering/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Document(models.Model):
+    """
+    A document uploaded by a user.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    file = models.FileField(upload_to='documents/')
+    extracted_text = models.TextField()
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Embedding(models.Model):
+    """
+    A document embedding.
+    """
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    vector = models.JSONField()
+
+    def __str__(self):
+        return f"Embedding for {self.document.title}"
