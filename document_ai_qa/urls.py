1e8325532c8e5b7d841a3765e9b21adc46821e75
shamspias <shamsuddinpias0@gmail.com>
2023-04-02 17:16:23 +0600

    fix: urls

--- document_ai_qa/urls.py
+++ document_ai_qa/urls.py
@@ -3,7 +3,24 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="chatbot builder API",
+        default_version='v1',
+        description="API for chatbot builder",
+        terms_of_service="https://github.com/shamspias",
+        contact=openapi.Contact(email="shamspias0@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny, ],
+)
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('api/', include('question_answering.urls')),
+                  path('api/', include('question_answering.urls'), name='api'),
+                  path('auth/', include('authentication.urls'), name='auth'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
