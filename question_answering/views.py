7373939609be64aff9f320aef62d194b1f3cb29a
shamspias <shamsuddinpias0@gmail.com>
2023-04-03 20:52:42 +0600

    fix: views

--- question_answering/views.py
+++ question_answering/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Conversation
+from .serializers import ConversationSerializer
 
-# Create your views here.
+
+class ConversationListView(generics.ListCreateAPIView):
+    """
+    View for listing and creating conversations.
+    """
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class ConversationDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    View for retrieving, updating, and deleting conversations.
+    """
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
