b9f9e2e963b64225332b4321d3f3660504ba0077
shamspias <shamsuddinpias0@gmail.com>
2023-04-03 23:13:01 +0600

    fix: admin

--- document_processing/admin.py
+++ document_processing/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Document
 
-# Register your models here.
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    """
+    Document model admin configuration.
+    """
+    list_display = ('user', 'document_type', 'pinecone_index_id')
+    search_fields = ('user__username', 'document_type', 'pinecone_index_id')
