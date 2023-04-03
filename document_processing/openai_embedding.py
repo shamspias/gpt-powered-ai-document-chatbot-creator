6d7792567152aa1ba8a3c12a8f023b96e31c6403
shamspias <shamsuddinpias0@gmail.com>
2023-04-04 02:28:44 +0600

    add embedding

--- document_processing/openai_embedding.py
+++ document_processing/openai_embedding.py
@@ -1,4 +1,13 @@
+import openai
+from django.conf import settings
+
+openai.api_key = settings.OPENAI_API_KEY
+
 
 def embed_text(text):
-    # Replace this mock implementation with the actual Text-Embedding-ADA-002 implementation when it becomes available.
-    return text
+    response = openai.Embedding.create(
+        input="Your text string goes here",
+        model="text-embedding-ada-002"
+    )
+    embeddings = response['data'][0]['embedding']
+    return embeddings
