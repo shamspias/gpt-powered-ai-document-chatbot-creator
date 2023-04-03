32ff3e05c9b0c5a5290dafabe1d55b721c82c7b9
shamspias <shamsuddinpias0@gmail.com>
2023-04-04 00:40:52 +0600

    fix: convert pdf to text

--- document_processing/utils.py
+++ document_processing/utils.py
@@ -1,9 +1,18 @@
-def convert_pdf_to_text(file):
+import PyPDF2
+
+
+def convert_pdf_to_text(file_path):
     """
-    Convert a PDF file to plain text.
+    Convert a PDF file to text.
+    :param file_path: Path to the PDF file
+    :return:  Text extracted from the PDF file
     """
-    # Replace this with your actual PDF-to-text conversion logic
-    return "Sample text extracted from the PDF file"
+    with open(file_path, 'rb') as file:
+        reader = PyPDF2.PdfFileReader(file)
+        text = ''
+        for page_num in range(reader.numPages):
+            text += reader.getPage(page_num).extractText()
+    return text
 
 
 def scrape_website_text(url):
