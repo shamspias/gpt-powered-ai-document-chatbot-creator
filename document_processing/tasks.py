d4e436ebb01a8f698c75ea7e24d64c5e21aeb1fd
shamspias <shamsuddinpias0@gmail.com>
2023-04-04 02:47:42 +0600

    fix: task pinecone list index

--- document_processing/tasks.py
+++ document_processing/tasks.py
@@ -5,13 +5,17 @@ from celery import shared_task
 from .utils import extract_text_from_file
 from .openai_embedding import embed_text
 
-pinecone_client = pinecone.init(api_key=settings.PINECONE_API_KEY, environment=settings.PINECONE_ENVIRONMENT)
+# Initialize Pinecone
+pinecone.init(api_key=settings.PINECONE_API_KEY, environment=settings.PINECONE_ENVIRONMENT)
+
+# Get a list of Pinecone indexes
+active_indexes = pinecone.list_indexes()
 
 # Check if the Pinecone index exists
-index_exists = settings.PINECONE_INDEX_NAME in pinecone_client.list_indexes()
+index_exists = settings.PINECONE_INDEX_NAME in active_indexes
 
 # Create the Pinecone index if it doesn't exist
-if index_exists:
+if not index_exists:
     pinecone.create_index(name=settings.PINECONE_INDEX_NAME, dimension=1536, metric="cosine")
 
 
