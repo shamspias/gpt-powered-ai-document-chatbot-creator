6681eb9389c078ed66ce4422155386b1bbac116a
shamspias <shamsuddinpias0@gmail.com>
2023-04-03 20:44:32 +0600

    feat: views

--- chatbot_management/views.py
+++ chatbot_management/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Chatbot
+from .serializers import ChatbotSerializer
 
-# Create your views here.
+
+class ChatbotListView(generics.ListCreateAPIView):
+    """
+    View for listing and creating chatbots.
+    """
+    queryset = Chatbot.objects.all()
+    serializer_class = ChatbotSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class ChatbotDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    View for retrieving, updating, and deleting chatbots.
+    """
+    queryset = Chatbot.objects.all()
+    serializer_class = ChatbotSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
